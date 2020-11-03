255f9909840181394f4dd29dde5f1dd42d2de67a
wyao <289672494@qq.com>
2020-11-03 22:25:23 +0800

    [wang yao] feature: client dto

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/dto/ClientDTO.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/dto/ClientDTO.java
@@ -1,4 +1,24 @@
 package com.thoughtworks.capacity.gtb.mvc.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientDTO {
+    @NotBlank
+    @Min(3)
+    @Max(10)
+    @Pattern(regexp = "^\\w+$")
+    private String username;
+    @NotBlank
+    @Min(5)
+    @Max(12)
+    private String password;
+    @Email
+    private String email;
 }
