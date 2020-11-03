d4c6f9391cebfc5b014c7d0e0d436da3e89fac51
wyao <289672494@qq.com>
2020-11-03 22:28:02 +0800

    [wang yao] feature: client data

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/data/ClientData.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/data/ClientData.java
@@ -1,4 +1,21 @@
 package com.thoughtworks.capacity.gtb.mvc.data;
 
+import com.thoughtworks.capacity.gtb.mvc.entity.ClientEntity;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class ClientData {
+
+    private static ClientData instance;
+    private List<ClientEntity> clients = new ArrayList<>();
+
+    public static ClientData getInstance() {
+        if (instance == null) {
+            instance = new ClientData();
+        }
+        return instance;
+    }
 }
