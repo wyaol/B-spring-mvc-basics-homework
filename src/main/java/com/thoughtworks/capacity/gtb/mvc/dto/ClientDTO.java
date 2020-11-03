17d1a499f243d0b61ee65b7e3c635efa1a93543a
wyao <289672494@qq.com>
2020-11-03 23:08:14 +0800

    [wang yao] feature: client dto 错误提示

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/dto/ClientDTO.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/dto/ClientDTO.java
@@ -10,15 +10,15 @@ import javax.validation.constraints.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClientDTO {
-    @NotBlank
-    @Min(3)
-    @Max(10)
-    @Pattern(regexp = "^\\w+$")
+    @NotBlank(message = "用户名不能为空")
+    @Min(value = 3, message = "用户名最低为3位")
+    @Max(value = 10, message = "用户名最高为10位")
+    @Pattern(regexp = "^\\w+$", message = "用户名只能包含字母，数字或下划线")
     private String username;
-    @NotBlank
-    @Min(5)
-    @Max(12)
+    @NotBlank(message = "密码不能为空")
+    @Min(value = 5, message = "密码最低为5位")
+    @Max(value = 12, message = "密码最多为12位")
     private String password;
-    @Email
+    @Email(message = "不符合邮箱格式")
     private String email;
 }
