ecd0e07b032061e90227d228ba9f76f945e258e5
wyao <289672494@qq.com>
2020-11-03 22:18:21 +0800

    [wang yao] feature: client entity

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/entity/ClientEntity.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/entity/ClientEntity.java
@@ -1,4 +1,15 @@
 package com.thoughtworks.capacity.gtb.mvc.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientEntity {
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
 }
