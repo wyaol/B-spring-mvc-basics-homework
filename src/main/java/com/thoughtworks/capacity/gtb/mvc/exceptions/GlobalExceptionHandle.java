a1ffa9afc91feb82f1588efd4be126d3cde222f3
wyao <289672494@qq.com>
2020-11-03 23:09:43 +0800

    [wang yao] fix: handle 请求参数不合法异常 补充ControllerAdvice注解

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/exceptions/GlobalExceptionHandle.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/exceptions/GlobalExceptionHandle.java
@@ -2,10 +2,12 @@ package com.thoughtworks.capacity.gtb.mvc.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ControllerAdvice
 public class GlobalExceptionHandle {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
